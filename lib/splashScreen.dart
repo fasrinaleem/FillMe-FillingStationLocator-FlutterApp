7e3614552da2169e19ff595d041db0a7e4fff6ae
Fasrin <fasrinaleem@gmail.com>
2020-04-05 22:38:45 +0530

    Circle button getting smaller in size when pressed and Button goes left side corner

--- lib/splashScreen.dart
+++ lib/splashScreen.dart
@@ -8,7 +8,41 @@ class SplashScreen extends StatefulWidget {
   _SpachScreenState createState() => _SpachScreenState();
 }
 
-class _SpachScreenState extends State<SplashScreen> {
+class _SpachScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  AnimationController _scaleController;
+  AnimationController _scale2Controller;
+  AnimationController _widthController;
+  AnimationController _positionController;
+
+  Animation<double> _scaleAnimation;
+  Animation<double> _scale2Animation;
+  Animation<double> _widthAnimation;
+  Animation<double> _positionAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _scaleController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+
+    _scaleAnimation =
+        Tween<double>(begin: 1.0, end: 0.8).animate(_scaleController)
+          ..addStatusListener((status) {
+            //keep the button small size when it pressed --> 3
+            if (status == AnimationStatus.completed) {
+              _widthController.forward();
+            }
+          });
+
+    _widthController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 600));
+
+    _widthAnimation =
+        Tween<double>(begin: 80.0, end: 300.0).animate(_widthController);
+  }
+
   Widget build(BuildContext context) {
     final double width = MediaQuery.of(context).size.width;
 
@@ -80,7 +114,7 @@ class _SpachScreenState extends State<SplashScreen> {
                   FadeAnimation(
                       1.3,
                       Text(
-                        "Testing text comes here.. we can add what ever we wants to",
+                        "Testing text comes here.. we can add what \n ever we wants to",
                         style: TextStyle(
                             color: Colors.white.withOpacity(.7), height: 1.4),
                       )),
@@ -89,16 +123,57 @@ class _SpachScreenState extends State<SplashScreen> {
                   ),
                   FadeAnimation(
                       1.6,
-                      Center(
-                        child: Container(
-                          width: 80,
-                          height: 80,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(50),
-                              color: Color.fromRGBO(0, 214, 227, 1)
-                                  .withOpacity(.4)),
-                        ),
-                      ))
+                      //Refoctoring centor into wrap padding to add the animation --> 1
+                      // Center(
+                      AnimatedBuilder(
+                          animation: _scaleController,
+                          builder: (context, child) => Transform.scale(
+                                scale: _scaleAnimation.value,
+                                child: Center(
+                                  child: AnimatedBuilder(
+                                    animation: _widthController,
+                                    builder: (context, child) => Container(
+                                      //Change the width based on animation (width size becomes bigger from 60 to 80) --> 4
+                                      // child: Container(
+                                      // width: 80,
+                                      width: _widthAnimation.value,
+                                      height: 80,
+                                      padding: EdgeInsets.all(10),
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(50),
+                                          color: Color.fromRGBO(0, 214, 227, 1)
+                                              .withOpacity(.4)),
+                                      child: InkWell(
+                                        //button is animated to forward (Button size getting smaller)  --> 2
+                                        onTap: () {
+                                          _scaleController.forward();
+                                        },
+                                        child: Stack(children: <Widget>[
+                                          Positioned(
+                                            left:
+                                                0, //Place the circle button into left side corner when it pressed --> 5
+                                            child: Container(
+                                              width: 60,
+                                              height: 60,
+                                              decoration: BoxDecoration(
+                                                  shape: BoxShape.circle,
+                                                  color: Color.fromRGBO(
+                                                      0, 214, 227, 1)),
+                                              child: Icon(Icons.arrow_forward,
+                                                  color: Colors.white),
+                                            ),
+                                          ),
+                                        ]),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ))),
+                  //put the text to the top
+                  SizedBox(
+                    height: 60,
+                  ),
                 ],
               ),
             )
